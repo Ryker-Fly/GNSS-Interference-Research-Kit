1ac15b17f7e32bb272a07a4a2923dae58b58a443
matlabfly <matlabfly@hotmail.com>
2026-03-17 15:24:10 +0800

    Remove test_check directory

--- test_check/girk/mitigation/__init__.py
+++
@@ -1,9 +0,0 @@
-﻿# girk/mitigation/__init__.py
-from .fdpb import fdpb
-from .notch_filter import iir_notch, fir_notch
-
-__all__ = [
-    "fdpb",
-    "iir_notch",
-    "fir_notch"
-]
--- test_check/girk_pro/__init__.py
+++
@@ -1,4 +0,0 @@
-# girk_pro/__init__.py
-from .fdpb_no_threshold import fdpb_no_threshold
-
-__all__ = ["fdpb_no_threshold"]
